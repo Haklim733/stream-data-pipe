075b77e45b8a70aa4e018feb0585b0f092d9f848
leehaklim <lee.haklim@gmail.com>
2025-02-18 18:35:34 -0800

    migrate to simple svelte app to rvisualize iceberg tables

---
+++ packages/ui/src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { onMount } from "svelte";
+
+  let tables = [];
+
+  onMount(async () => {
+    const response = await fetch(`${import.meta.env.VITE_ICEBERG_URL}/tables`);
+    const data = await response.json();
+    tables = data;
+  });
+</script>
+
+<h1>Iceberg Tables</h1>
+<ul>
+  {#each tables as table}
+    <li>{table.name}</li>
+  {/each}
+</ul>
