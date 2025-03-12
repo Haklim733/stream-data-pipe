f11927a93b35fc48c39277d1b030f1fff846d449
leehaklim <lee.haklim@gmail.com>
2025-03-12 01:00:26 -0700

    work on generating drone telemetry

--- services/kafka/src/models.py
+++ services/kafka/src/models.py
@@ -1,340 +1,203 @@
-# https://github.com/jaehyeon-kim/flink-demos/blob/master/building-pyflink-apps/src/models.py
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
 import datetime
-import dataclasses
+import json
+import logging
+import random
+import time
+import typing
+import uuid
 
-from pyflink.common import Types, Row
 
-from utils import serialize
+from kafka import KafkaAdminClient, KafkaProducer
+from kafka.admin import NewTopic
+from kafka.errors import (
+    KafkaError,
+    UnknownTopicOrPartitionError,
+    TopicAlreadyExistsError,
+)
+
+logger = logging.getLogger(__name__)
 
 
-@dataclasses.dataclass
-class FlightData:
-    email_address: str
-    departure_time: str
-    departure_airport_code: str
-    arrival_time: str
-    arrival_airport_code: str
-    flight_number: str
-    confirmation: str
-    source: str
+class KafkaClient:
+    def __init__(self, bootstrap_servers: str) -> None:
+        self.bootstrap_servers = bootstrap_servers
+        self.admin = self.create_admin()
+        self.producer = self.create_producer()
 
-    def get_duration(self):
-        return int(
-            (
-                datetime.datetime.fromisoformat(self.arrival_time)
-                - datetime.datetime.fromisoformat(self.departure_time)
-            ).seconds
-            / 60
+    def create_admin(self):
+        return KafkaAdminClient(bootstrap_servers=self.bootstrap_servers)
+
+    def create_producer(self):
+        return KafkaProducer(
+            bootstrap_servers=self.bootstrap_servers,
+            key_serializer=lambda v: json.dumps(v).encode("utf-8"),
+            value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+            acks="all",
+            retries=3,
+            retry_backoff_ms=100,
+            request_timeout_ms=3000,  # 30 sec
         )
 
-    def to_row(self):
-        return Row(
-            email_address=self.email_address,
-            departure_time=serialize(self.departure_time),
-            departure_airport_code=self.departure_airport_code,
-            arrival_time=serialize(self.arrival_time),
-            arrival_airport_code=self.arrival_airport_code,
-            flight_number=self.flight_number,
-            confirmation=self.confirmation,
-            source=self.source,
+    def delete_topics(self, topic_names: typing.List[str]):
+        for name in topic_names:
+            try:
+                self.admin_client.delete_topics([name], timeout_ms=1000)
+            except UnknownTopicOrPartitionError:
+                pass
+            except Exception as err:
+                raise RuntimeError(f"fails to delete topic - {name}") from err
+
+    def create_topics(self, topics: typing.List[NewTopic], to_recreate: bool = True):
+        if to_recreate:
+            self.delete_topics([t.name for t in topics])
+        for topic in topics:
+            try:
+                resp = self.admin_client.create_topics([topic])
+                name, error_code, error_message = resp.topic_errors[0]
+                logger.info(
+                    f"topic created, name - {name}, error code - {error_code}, error message - {error_message}"
+                )
+            except TopicAlreadyExistsError:
+                pass
+            except KafkaError as err:
+                raise RuntimeError(
+                    f"fails to create topics - {', '.join(t.name for t in topics)}"
+                ) from err
+        logging.info(
+            f"topics created successfully - {', '.join([t.name for t in topics])}"
         )
 
-    @classmethod
-    def from_row(cls, row: Row):
-        return cls(
-            email_address=row.email_address,
-            departure_time=row.departure_time,
-            departure_airport_code=row.departure_airport_code,
-            arrival_time=row.arrival_time,
-            arrival_airport_code=row.arrival_airport_code,
-            flight_number=row.flight_number,
-            confirmation=row.confirmation,
-            source=row.source,
-        )
-
-    @staticmethod
-    def get_key_type_info():
-        return Types.ROW_NAMED(
-            field_names=[
-                "confirmation",
-            ],
-            field_types=[
-                Types.STRING(),
-            ],
-        )
-
-    @staticmethod
-    def get_value_type_info():
-        return Types.ROW_NAMED(
-            field_names=[
-                "email_address",
-                "departure_time",
-                "departure_airport_code",
-                "arrival_time",
-                "arrival_airport_code",
-                "flight_number",
-                "confirmation",
-                "source",
-            ],
-            field_types=[
-                Types.STRING(),
-                Types.STRING(),
-                Types.STRING(),
-                Types.STRING(),
-                Types.STRING(),
-                Types.STRING(),
-                Types.STRING(),
-                Types.STRING(),
-            ],
-        )
-
-    @staticmethod
-    def to_user_statistics_data(row: Row):
-        data = FlightData.from_row(row)
-        return UserStatistics(data.email_address, data.get_duration(), 1)
+    def send_items(self, topic_name: str, item: dict[str, typing.Any]):
+        try:
+            self.producer_client.send(
+                topic=topic_name,
+                value=item,
+            )
+            logging.info(f"record sent, topic - {topic_name}, ref - {key['ref']}")
+        except Exception as err:
+            raise RuntimeError("fails to send a message") from err
 
 
-@dataclasses.dataclass
-class BaseStatistics:
-    email_address: str
-    total_flight_duration: int
-    number_of_flights: int
+class IoTTelemetry(ABC):
+    def __init__(
+        self,
+        max_time: int,
+    ):
+        self.max_time: int = max_time
+
+    @abstractmethod
+    def generate(self):
+        raise NotImplementedError
 
 
-@dataclasses.dataclass
-class UserStatistics(BaseStatistics):
-    def to_row(self):
-        return Row(
-            email_address=self.email_address,
-            total_flight_duration=self.total_flight_duration,
-            number_of_flights=self.number_of_flights,
-        )
+class IoTDevice(ABC):
+    def __init__(self, max_time: int, datatype: IoTTelemetry, client: KafkaClient):
+        self.max_time: int = max_time
+        self.datatype = datatype
+        self.client = client
 
-    @staticmethod
-    def merge(this: BaseStatistics, that: BaseStatistics):
-        assert this.email_address == that.email_address
-        return UserStatistics(
-            email_address=this.email_address,
-            total_flight_duration=this.total_flight_duration + that.total_flight_duration,
-            number_of_flights=this.number_of_flights + that.number_of_flights,
-        )
-
-    @classmethod
-    def from_flight(cls, data: FlightData):
-        return cls(
-            email_address=data.email_address,
-            total_flight_duration=data.get_duration(),
-            number_of_flights=1,
-        )
-
-    @classmethod
-    def from_row(cls, row: Row):
-        return cls(
-            email_address=row.email_address,
-            total_flight_duration=row.total_flight_duration,
-            number_of_flights=row.number_of_flights,
-        )
-
-    @staticmethod
-    def get_key_type_info():
-        return Types.ROW_NAMED(
-            field_names=[
-                "email_address",
-            ],
-            field_types=[
-                Types.STRING(),
-            ],
-        )
-
-    @staticmethod
-    def get_value_type_info():
-        return Types.ROW_NAMED(
-            field_names=[
-                "email_address",
-                "total_flight_duration",
-                "number_of_flights",
-            ],
-            field_types=[
-                Types.STRING(),
-                Types.INT(),
-                Types.INT(),
-            ],
-        )
+    @abstractmethod
+    def generate(self):
+        raise NotImplementedError
 
 
-@dataclasses.dataclass
-class SkyoneData:
-    email_address: str
-    flight_departure_time: str
-    iata_departure_code: str
-    flight_arrival_time: str
-    iata_arrival_code: str
-    flight_number: str
-    confirmation: str
-    ticket_price: int
-    aircraft: str
-    booking_agency_email: str
+class DroneTelemetry(IoTTelemetry):
+    def __init__(self):
+        self.device: str = uuid.uuid4()
+        self.timestamp: int = datetime.datetime.now().timestamp()
+        self.location: Location = Location()
+        self.altitude: float = round(random.uniform(0, 1000), 1)
+        self.speed: float = round(random.uniform(3, 60), 1)
+        self.batteryVoltage: int = random.randint(15, 100)
 
-    def asdict(self):
-        return dataclasses.asdict(self)
+    def generate(self) -> dict[str, typing.Any]:
+        """
+        Generates a DroneTelemetry instance with random values for each field.
+        """
+        telemetry_data = {}
+        for key, value in self.__dict__.items():
+            if not key.startswith("_") and key not in ["device", "location"]:
+                telemetry_data[key] = self._generate_random_value(key, value)
+        telemetry_data["device"] = self.device
+        telemetry_data["location"] = self.location.generate()
+        return telemetry_data
 
-    @classmethod
-    def from_row(cls, row: Row):
-        return cls(
-            email_address=row.email_address,
-            flight_departure_time=row.flight_departure_time,
-            iata_departure_code=row.iata_departure_code,
-            flight_arrival_time=row.flight_arrival_time,
-            iata_arrival_code=row.iata_arrival_code,
-            flight_number=row.flight_number,
-            confirmation=row.confirmation,
-            ticket_price=row.ticket_price,
-            aircraft=row.aircraft,
-            booking_agency_email=row.booking_agency_email,
-        )
-
-    def to_row(self):
-        return Row(
-            email_address=self.email_address,
-            flight_departure_time=serialize(self.flight_departure_time),
-            iata_departure_code=self.iata_departure_code,
-            flight_arrival_time=serialize(self.flight_arrival_time),
-            iata_arrival_code=self.iata_arrival_code,
-            flight_number=self.flight_number,
-            confirmation=self.confirmation,
-            ticket_price=self.ticket_price,
-            aircraft=self.aircraft,
-            booking_agency_email=self.booking_agency_email,
-        )
-
-    @staticmethod
-    def get_value_type_info():
-        return Types.ROW_NAMED(
-            field_names=[
-                "email_address",
-                "flight_departure_time",
-                "iata_departure_code",
-                "flight_arrival_time",
-                "iata_arrival_code",
-                "flight_number",
-                "confirmation",
-                "ticket_price",
-                "aircraft",
-                "booking_agency_email",
-            ],
-            field_types=[
-                Types.STRING(),
-                Types.STRING(),
-                Types.STRING(),
-                Types.STRING(),
-                Types.STRING(),
-                Types.STRING(),
-                Types.STRING(),
-                Types.STRING(),
-                Types.STRING(),
-                Types.STRING(),
-            ],
-        )
-
-    @staticmethod
-    def to_flight_data(row: Row):
-        data = SkyoneData.from_row(row)
-        return FlightData(
-            data.email_address,
-            data.flight_departure_time,
-            data.iata_departure_code,
-            data.flight_arrival_time,
-            data.iata_arrival_code,
-            data.flight_number,
-            data.confirmation,
-            "skyone",
-        )
+    def _generate_random_value(
+        self, field_name: str, field_value: typing.Any
+    ) -> typing.Any:
+        if field_name == "location":
+            return Location.generate()
+        elif field_name == "timestamp":
+            timestamp = datetime.datetime.now().timestamp()
+            self.timestamp = timestamp
+            return timestamp
+        elif field_name == "altitude":
+            altitude = round(self.altitude + random.uniform(0.0, 1.0), 2)
+            self.altitude = altitude
+            return altitude
+        elif field_name == "speed":
+            speed = self.speed + round(random.uniform(0.0, 1.0), 2)  # meters
+            self.speed = speed
+            return speed
+        else:
+            return field_value
 
 
-@dataclasses.dataclass
-class SunsetData:
-    customer_email_address: str
-    departure_time: str
-    departure_airport: str
-    arrival_time: str
-    arrival_airport: str
-    flight_duration: int
-    flight_id: str
-    reference_number: str
-    total_price: int
-    aircraft_details: str
+class Drone(IoTDevice):
+    def __init__(self, max_time: int, data_type: DroneTelemetry, client: KafkaClient):
+        self.max_time: int = max_time
+        self.data_type = data_type
+        self.client = client
 
-    def asdict(self):
-        return dataclasses.asdict(self)
+    def generate(self):
+        start_time = time.time()
+        while time.time() - start_time < self.max_time:
+            data = self.data_type.generate()
+            print(data)
+            self.client.send_items(topic_name="drone", item=data)
+            # Sleep for a random interval between 0.5 to 2 seconds
+            sleep_time = random.uniform(0.2, 2.0)
+            time.sleep(sleep_time)
 
-    @classmethod
-    def from_row(cls, row: Row):
-        return cls(
-            customer_email_address=row.customer_email_address,
-            departure_time=row.departure_time,
-            departure_airport=row.departure_airport,
-            arrival_time=row.arrival_time,
-            arrival_airport=row.arrival_airport,
-            flight_duration=row.flight_duration,
-            flight_id=row.flight_id,
-            reference_number=row.reference_number,
-            total_price=row.total_price,
-            aircraft_details=row.aircraft_details,
-        )
 
-    def to_row(self):
-        return Row(
-            customer_email_address=self.customer_email_address,
-            departure_time=serialize(self.departure_time),
-            departure_airport=self.departure_airport,
-            arrival_time=serialize(self.arrival_time),
-            arrival_airport=self.arrival_airport,
-            flight_duration=self.flight_duration,
-            flight_id=self.flight_id,
-            reference_number=self.reference_number,
-            total_price=self.total_price,
-            aircraft_details=self.aircraft_details,
-        )
+class Location:
+    def __init__(self):
+        self.latitude: int = 34.1184
+        self.longitude: int = -118.3004
 
-    @staticmethod
-    def get_value_type_info():
-        return Types.ROW_NAMED(
-            field_names=[
-                "customer_email_address",
-                "departure_time",
-                "departure_airport",
-                "arrival_time",
-                "arrival_airport",
-                "flight_duration",
-                "flight_id",
-                "reference_number",
-                "total_price",
-                "aircraft_details",
-            ],
-            field_types=[
-                Types.STRING(),
-                Types.STRING(),
-                Types.STRING(),
-                Types.STRING(),
-                Types.STRING(),
-                Types.STRING(),
-                Types.STRING(),
-                Types.STRING(),
-                Types.STRING(),
-                Types.STRING(),
-            ],
-        )
+    def offset_coordinates_by_meter(self, seconds=1):
+        # Approximate values for 1 meter in degrees
+        meter_in_degrees_lat = 0.000009
+        meter_in_degrees_lon = 0.000009
+        offset_lat = meter_in_degrees_lat * seconds
+        offset_lon = meter_in_degrees_lon * seconds
+        new_lat = self.latitude + offset_lat
+        new_lon = self.longitude + offset_lon
+        return new_lat, new_lon
 
-    @staticmethod
-    def to_flight_data(row: Row):
-        data = SunsetData.from_row(row)
-        return FlightData(
-            data.customer_email_address,
-            data.departure_time,
-            data.departure_airport,
-            data.arrival_time,
-            data.arrival_airport,
-            data.flight_id,
-            data.reference_number,
-            "sunset",
-        )
+    def generate(self) -> dict[str, float]:
+        lat, lon = self.offset_coordinates_by_meter()
+        self.latitude, self.longitude = lat, lon
+        return {"latitude": lat, "longitude": lon}
+
+
+@dataclass
+class PathResults:
+    paths: list[Location]
+    totalDistance: int
+
+
+@dataclass
+class TelemetryResults:
+    totalTime: int
+    totalDistance: int
+
+
+if __name__ == "__main__":
+    from unittest.mock import MagicMock
+
+    client = MagicMock()
+    device = Drone(max_time=2, data_type=DroneTelemetry(), client=client)
+    device.generate()
